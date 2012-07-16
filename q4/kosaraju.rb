61a08a97dae2f0ba1c1f4cfb87c0bac783459967
jjhageman <jjhageman@gmail.com>
2012-07-16 01:11:44 -0700

    dfs

--- q4/kosaraju.rb
+++ q4/kosaraju.rb
@@ -1,33 +1,71 @@
-require 'set'
-require 'csv'
-require 'debugger'
+#require 'set'
+#require 'csv'
+#require 'debugger'
+require 'rubygems'
+require 'fastercsv'
 
-@explored = []
-@adj_nodes = {}
+@explored=[]
+@adj_nodes={}
+@rev_nodes={}
 @time=0
+@finish_times={}
 @leaders={}
 @source=0
 
 def kosaraju(g)
-  g.inject(@adj_nodes) do |sum, e|
-    sum.has_key?(e[0]) ? (sum[e[0]] << e[1]) : (sum[e[0]]=[e[1]])
-    sum
-  end
-  @adj_nodes.each_key {|v| @source=v; dfs(v)}
-  puts "leaders: #{@leaders}, explored: #{@explored.sort}"
-end
-
-def dfs(s)
-  unless @explored.include?(s)
-    @explored << s
-    @leaders[s]=@source
+  g.each do |e|
+    @adj_nodes.has_key?(e[0]) ? (@adj_nodes[e[0]] << e[1]) : (@adj_nodes[e[0]]=[e[1]])
+    @rev_nodes.has_key?(e[1]) ? (@rev_nodes[e[1]] << e[0]) : (@rev_nodes[e[1]]=[e[0]])
   end
 
-  @adj_nodes[s].each do |v|
-    dfs(v) unless @explored.include?(v) 
-  end 
+  #@n=1000
+  @n=875714
+  i=@n
+  #puts "revg: #{@rev_nodes}"
+  while i > 0
+    @source=i
+    dfs(@rev_nodes, i)
+    i -= 1
+  end
+
+  #puts "finishing times: #{@finish_times}"
+
+  @leaders={}
+  @explored=[]
+  times = @finish_times.clone
+
+  i=@n
+  while i > 0
+    @source = i
+   dfs(@adj_nodes, times.key(i))
+   i -= 1
+  end
+  puts "end"
+  counts = @leaders.group_by{|a,b| b}
+  sizes = counts.values.map{|v| v.size}
+  #puts "leaders: #{@leaders}"
+  puts "sizes: #{sizes.sort.reverse[0..5]}"
 end
 
-g=CSV.read("small.txt", :col_sep => " ", :converters => :numeric).sort
+def dfs(g,s)
+  #puts "g: #{g}, s: #{s}"
+
+  return if @explored.include?(s)
+
+  @explored << s
+  #puts "s: #{s}, source: #{@source}"
+  @leaders[s]=@source
+
+  if g[s]
+    g[s].each do |v|
+      dfs(g,v) unless @explored.include?(v)
+    end 
+  end
+
+  @time += 1
+  @finish_times[s]=@time
+end
+
+g=FasterCSV.read("SCC.txt", :col_sep => " ", :converters => :numeric)
 
 kosaraju g
